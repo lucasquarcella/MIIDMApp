866306301d6bf05bc6902257131443e654d1b7ba
Michele Leggieri <50110582+MikeLeg01@users.noreply.github.com>
2020-03-06 11:27:30 +0100

    Add files via upload

---
+++ prova_navbar.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>prova nav bar</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <link rel="stylesheet" href="navbar.css">
+  </head>
+  <body>
+    <?php include("navbar.html") ?>
+  </body>
+</html>
